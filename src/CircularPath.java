26e09ddfa2b3c07365e570ea6ea06e085338836c
MaxHome <maxator@yandex.ru>
2023-10-25 18:59:25 +0300

    optimised

--- src/CircularPath.java
+++ src/CircularPath.java
@@ -9,42 +9,30 @@ public class CircularPath {
         ArrayList<Integer> iterationArray = new ArrayList<>();
         ArrayList<List<Integer>> finalArrayOfArrays = new ArrayList<>();
 
-
-
         for (int j = 1; j <= n; j++) {
             circularArray.add(j);
         }
 
         int index = 1;
         int lastDigit = 0;
+
         while (lastDigit != 1) {
             index--;
             if (index == -1) index = m;
 
             for (int i = 0; i < m; i++) {
                 iterationArray.add(circularArray.get(index));
-                index = getNextIndex(circularArray, index);
+                index = (index + 1) % n;
             }
             lastDigit = iterationArray.get(iterationArray.size()-1);
             finalArrayOfArrays.add(new ArrayList<>(iterationArray));
             iterationArray.clear();
-
         }
 
         for (List<Integer> finalArrayOfArray : finalArrayOfArrays) {
             System.out.print(finalArrayOfArray.get(0));
         }
 
-
     }
-
-    private static int getNextIndex(ArrayList<Integer> array, int current) {
-        if (current < array.size() - 1) {
-            return current + 1;
-        } else {
-            return 0;
-        }
-    }
-
 
 }
